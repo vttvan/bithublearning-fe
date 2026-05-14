1917f523b6bacc76859befae2bba8aedef87989b
vankit123 <kietvkse182744@fpt.edu.vn>
2026-05-14 14:39:22 +0700

    deploy github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-
+  base: '/bithublearning-fe/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
